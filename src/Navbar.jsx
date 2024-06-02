9f05283036a7f95de2fb63b5a70b271b1d7eb297
guillermo <martinezguillermo559@gmail.com>
2024-06-01 19:13:15 -0600

    Added a responsive navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,51 @@
 import "./index.css"
 import PdfResume from "./assets/docs/Resume_SoftwareE.pdf";
+import BurgerMenuIcon from "./assets/icons/burger-menu.svg";
+import CancelMenuIcon from "./assets/icons/cancel-menu.svg";
 
 
 export  default function Navbar(){
+
+    function showMenu(){
+        document.querySelector('body').classList.add('overflow-hidden');
+        document.querySelector('nav').classList.remove('-left-full');
+        document.querySelector('nav').classList.add('left-0');
+        document.querySelector('#showMenuBtn').classList.add('hidden');
+        document.querySelector('#hideMenuBtn').classList.remove('hidden')
+    }
+
+    function hideMenu(){
+        document.querySelector('body').classList.remove('overflow-hidden');
+        document.querySelector('nav').classList.remove('left-0');
+        document.querySelector('nav').classList.add('-left-full');
+        document.querySelector('#showMenuBtn').classList.remove('hidden');
+        document.querySelector('#hideMenuBtn').classList.add('hidden')
+    }
+
+    function clickMenuItem(){
+        document.querySelector('body').classList.remove('overflow-hidden');
+        document.querySelector('nav').classList.remove('left-0');
+        document.querySelector('nav').classList.add('-left-full');
+    }
+
+
     return (
-        <div className="navbar bg-primary px-12 xl:px-36">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl px-0">Portfolio Guillermo M.</a>
-        </div>
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1 font-medium text-lg">
-                {/*
-                <li><a href="#footer">Contact</a></li>
-                <li><a href={PdfResume} target="_blank">Resume</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-            
-                */} 
-            </ul>
-        </div>
+        <div className="navbar bg-primary px-6 xl:px-36">
+            <button id="showMenuBtn" className="md:hidden" onClick={showMenu}><img src={BurgerMenuIcon} ></img></button>
+            <button id="hideMenuBtn" className="hidden md:hidden" onClick={hideMenu}><img src={CancelMenuIcon} ></img></button>
+            <div className="flex-1 justify-center md:justify-start">
+                <a className="btn btn-ghost text-3xl px-0" href="https://unicomet.github.io/Portfolio/">Portfolio Guillermo M.</a>
+            </div>
+            <div className="flex-none">
+                <nav className="menu absolute top-16 px-6 py-12 -left-full bg-primary w-full h-full md:static md:py-4 md:px-1 md:flex transition-all">
+                    <ul className="flex flex-col md:flex-row justify-around items-center h-5/6 font-bold text-3xl md:text-xl">
+                        <li><a href="#footer" onClick={clickMenuItem} className="text-center">Contact</a></li>
+                        <li><a href={PdfResume} target="_blank" onClick={clickMenuItem} >Resume</a></li>
+                        <li><a href="#experience" onClick={clickMenuItem} >Experience</a></li>
+                        <li><a href="#projects" onClick={clickMenuItem} >Projects</a></li> 
+                    </ul>
+                </nav>
+            </div>
         </div>
     )
 }
