2c23afd71bda08e015c0115aceaa30e0f4d44f49
guillermo <martinezguillermo559@gmail.com>
2024-06-01 19:43:42 -0600

    selector to top navbar fixed and footer improved

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,24 +8,18 @@ export  default function Navbar(){
 
     function showMenu(){
         document.querySelector('body').classList.add('overflow-hidden');
-        document.querySelector('nav').classList.remove('-left-full');
-        document.querySelector('nav').classList.add('left-0');
+        document.querySelector('#topNavbar').classList.remove('-left-full');
+        document.querySelector('#topNavbar').classList.add('left-0');
         document.querySelector('#showMenuBtn').classList.add('hidden');
         document.querySelector('#hideMenuBtn').classList.remove('hidden')
     }
 
     function hideMenu(){
         document.querySelector('body').classList.remove('overflow-hidden');
-        document.querySelector('nav').classList.remove('left-0');
-        document.querySelector('nav').classList.add('-left-full');
-        document.querySelector('#showMenuBtn').classList.remove('hidden');
+        document.querySelector('#topNavbar').classList.remove('left-0');
+        document.querySelector('#topNavbar').classList.add('-left-full');
         document.querySelector('#hideMenuBtn').classList.add('hidden')
-    }
-
-    function clickMenuItem(){
-        document.querySelector('body').classList.remove('overflow-hidden');
-        document.querySelector('nav').classList.remove('left-0');
-        document.querySelector('nav').classList.add('-left-full');
+        document.querySelector('#showMenuBtn').classList.remove('hidden');
     }
 
 
@@ -34,15 +28,15 @@ export  default function Navbar(){
             <button id="showMenuBtn" className="md:hidden" onClick={showMenu}><img src={BurgerMenuIcon} ></img></button>
             <button id="hideMenuBtn" className="hidden md:hidden" onClick={hideMenu}><img src={CancelMenuIcon} ></img></button>
             <div className="flex-1 justify-center md:justify-start">
-                <a className="btn btn-ghost text-3xl px-0" href="https://unicomet.github.io/Portfolio/">Portfolio Guillermo M.</a>
+                <a className="btn btn-ghost text-2xl px-0" href="https://unicomet.github.io/Portfolio/">Portfolio Guillermo M.</a>
             </div>
             <div className="flex-none">
-                <nav className="menu absolute top-16 px-6 py-12 -left-full bg-primary w-full h-full md:static md:py-4 md:px-1 md:flex transition-all">
+                <nav id="topNavbar" className="menu absolute top-16 px-6 py-12 -left-full bg-primary w-full h-full md:static md:py-4 md:px-1 md:flex transition-all">
                     <ul className="flex flex-col md:flex-row justify-around items-center h-5/6 font-bold text-3xl md:text-xl">
-                        <li><a href="#footer" onClick={clickMenuItem} className="text-center">Contact</a></li>
-                        <li><a href={PdfResume} target="_blank" onClick={clickMenuItem} >Resume</a></li>
-                        <li><a href="#experience" onClick={clickMenuItem} >Experience</a></li>
-                        <li><a href="#projects" onClick={clickMenuItem} >Projects</a></li> 
+                        <li><a href="#footer" onClick={hideMenu} className="text-center">Contact</a></li>
+                        <li><a href={PdfResume} target="_blank" onClick={hideMenu} >Resume</a></li>
+                        <li><a href="#experience" onClick={hideMenu} >Experience</a></li>
+                        <li><a href="#projects" onClick={hideMenu} >Projects</a></li> 
                     </ul>
                 </nav>
             </div>
